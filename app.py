791ccad35a1bc48d3b2d4c7010866f3746f20844
BaptisteT31 <baptiste.teulier@gmail.com>
2025-03-11 05:21:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 CORS(app)
 
 # --- Configuration PostgreSQL ---
-DB_HOST = os.getenv("DB_HOST", "your_db_host")
-DB_NAME = os.getenv("DB_NAME", "your_db_name")
-DB_USER = os.getenv("DB_USER", "your_db_user")
-DB_PASS = os.getenv("DB_PASS", "your_db_password")
+DB_HOST = os.getenv("DB_HOST", "dpg-cutkc1d6l47c73a6kntg-a")
+DB_NAME = os.getenv("DB_NAME", "escape-game-db")
+DB_USER = os.getenv("DB_USER", "escape_game_db_user")
+DB_PASS = os.getenv("DB_PASS", "0mPOqxzc51tk0GCtyJH2kAx7dDugHbEp")
 DB_PORT = os.getenv("DB_PORT", "5432")
 
 # --- Fonction pour se connecter à la base ---
